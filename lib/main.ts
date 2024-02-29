5e99c453aa297e5a72f26a0707d0dd6e1f3e353c
임준혁 <wick@rsquare.co.kr>
2024-02-29 16:48:27 +0900

    base commit

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,8 @@
 export * from '@mui/material';
 export * as styles from '@mui/material/styles';
 export * as colors from '@mui/material/colors';
+export * as XDatePickers from '@mui/x-date-pickers';
+export * as AdapterDayjs from '@mui/x-date-pickers/AdapterDayjs';
 
 export { Row } from './components/Row';
 export { Column } from './components/Column';
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import { Button, Label, Input } from '../';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [inputCustomCountValue, setInputCustomCountValue] = useState('');
-
-  const handleClickCustomCount = () => {
-    if (inputCustomCountValue === '') {
-      setCount(count => count + 1);
-    } else {
-      setCount(Number(inputCustomCountValue));
-    }
-  }
-
   return (
     <>
       <div>
@@ -28,15 +15,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Label>My Label</Label><br />
-        <Input
-          placeholder="Custom count"
-          value={inputCustomCountValue}
-          onChange={(e) => setInputCustomCountValue(e.target.value)}
-        /><br />
-        <Button onClick={handleClickCustomCount}>
-          count is {count}
-        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
