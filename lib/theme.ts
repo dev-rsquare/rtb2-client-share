5e75deeec58a4d9f1df9cf2990bebfb42e9243a5
임준혁 <wick@rsquare.co.kr>
2024-03-12 10:45:57 +0900

    base commit

---
+++ lib/theme.ts
@@ -0,0 +1,36 @@
+
+import { red } from "./colors";
+import { createTheme } from "./main";
+
+import "pretendard/dist/web/static/pretendard.css";
+
+// A custom theme for this app
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#556cd6",
+        },
+        secondary: {
+            main: "#19857b",
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+    typography: {
+        fontFamily: "Pretendard",
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    ".Mui-disabled": {
+                        backgroundColor: "#EFF2F5",
+                    },
+                },
+            },
+        },
+    },
+});
+
+export default theme;
